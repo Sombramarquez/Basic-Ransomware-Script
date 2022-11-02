f3df68ddb60309434b5c8d28e8b18eccf57c1e5a
Sombramarquez <84882659+Sombramarquez@users.noreply.github.com>
2022-11-02 02:16:15 -0700

    Update Decryption.py

--- Decryption.py
+++ Decryption.py
@@ -5,50 +5,45 @@ The script encrypts and decrypts target files'
 
 """""
 _author_ = "Victor Marquez"
-_version_ =0.1
+_version_ =0.2
 
 import os
 from pathlib import Path
 from Crypto.Cipher import AES
 
-
+#Allows script to know what files to target
 def detectFiles(mainPath):
     for entry in os.scandir(mainPath):
         if entry.is_file():
             yield entry
 
 def decryptFile(file, Pkey):
-
-
     #Read file
     dataFIle = file
     with open(dataFIle, 'rb') as f:
         nonce, tag, cipheredtext = [f.read(x) for x in (16,16,-1)]
 
 
-
     #Decrypt the data with the key
-
     cipher = AES.new(Pkey, AES.MODE_EAX,nonce)
     text = cipher.decrypt_and_verify(cipheredtext,tag)
 
 
-    #Save data
+    #Save data into a new file
     fileName = file.name.removesuffix(file.suffix)
     fileExtension = ".decrypted"
-    encFile = "Folder Path"+fileName + fileExtension
+    encFile = "C:/Users/victo/Desktop/test_Folder/"+fileName + fileExtension
     with open(encFile, 'wb') as f:
         f.write(text)
         f.close()
         os.remove(file)
 
-
-
-
-for file in detectFiles("FILE PATH"):
-    print(file)
+        
+        
+        
+path = "Path where encrypted files are located"
+for file in detectFiles(path):
     filePath = Path(file)
     fileType = filePath.suffix.lower()
-    print(fileType)
     if fileType in ['.hacked!']:
-        decryptFile(filePath,b'Sixyeen byte key')
+        decryptFile(filePath,b'Sixyeen byte key') #Key, script will not work if keys are not the same
